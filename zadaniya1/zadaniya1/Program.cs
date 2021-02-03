0da3b654b7b0e515d06dd2a57173e6a0056bcb67
Muhammad <davlatovmuhammad1264@mail.com>
2021-02-03 15:00:09 +0500

    Add zadaniya1

---
+++ zadaniya1/zadaniya1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace zadaniya1
+{
+	class Calc
+	{
+		public int x=10;
+		public int y = 25;
+
+
+		public void GetInfo()
+		{
+			int z = x + y;
+			Console.WriteLine($"{x}-{y}-{z}");
+
+		}
+	}
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Calc calc = new Calc();
+
+			calc.GetInfo();
+			Console.ReadKey();
+		}
+	}
+}
